6d87356887dbd1559aea10d4a70ccf92d475950f
Stefan Fischer <stefischer@mail.com>
2024-09-27 05:31:17 -0400

    Create environment constants

---
+++ src/constants/environment.ts
@@ -0,0 +1,7 @@
+export const NOTION_SECRET = process.env.NOTION_SECRET as string;
+
+export const NOTION_SALES_DATABASE_ID = process.env.NOTION_SALES_DATABASE_ID as string;
+
+export const NOTION_TOTAL_SALES_BLOCK_ID = process.env.NOTION_TOTAL_SALES_BLOCK_ID as string;
+export const NOTION_ONLINE_SALES_BLOCK_ID = process.env.NOTION_ONLINE_SALES_BLOCK_ID as string;
+export const NOTION_EVENT_SALES_BLOCK_ID = process.env.NOTION_EVENT_SALES_BLOCK_ID as string;
